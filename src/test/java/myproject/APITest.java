d1ac5e4eb50edec288d3fb4c7910173c6a7e92fd
Samrat Nag <SamratY92@gmail.com>
2018-09-29 12:26:54 +0530

    First Commit

---
+++ src/test/java/myproject/APITest.java
@@ -0,0 +1,16 @@
+package myproject;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void postALM()
+	{
+		System.out.println("postALM");
+	}
+	@Test
+	public void postJIRA()
+	{
+		System.out.println("postJIRA");
+	}
+}
---
+++ src/test/java/myproject/SeleniumTest.java
@@ -0,0 +1,17 @@
+package myproject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void Browser()
+	{
+		System.out.println("Browser");
+	}
+	
+	@Test
+	public void UI()
+	{
+		System.out.println("UI");
+	}
+}
